321b54291de4f952e3743d3a619e52c8ade3244a
Steve Anderson <prototype909@msn.com>
2020-06-19 01:32:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,12 +8,12 @@ class App < Sinatra::Base
   end
 
   get '/' do
-  erb :'index'
+  erb :index
   end
 
   post '/checkout' do
     @item = params[:item]
     session["item"] = @item
-  erb :'checkout'
+  erb :checkout
 end
 end
